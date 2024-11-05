427079d24c87ac0677d8cf49d3c16314ee001c79
Monkeylust <mo9keylust@mail.ru>
2024-11-06 00:19:22 +0300

    Изменения: 1. Создал вариант с обычным массивом и динамическим массивом (динамический закоменчен). 2. Додумался в обычном массиве создавать массив длинной сплитового массива через numbersString.length. 3. Передаю файл в Main. В процессе: 4. Создать свое исключение.

--- src/Day14_Read_from_file_Exceptions/Task01/Main.java
+++ src/Day14_Read_from_file_Exceptions/Task01/Main.java
@@ -4,31 +4,38 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        printSumDigits(new File("temp"));
+        printSumDigits(new File("Day14Task01.txt"));
     }
 
     public static void printSumDigits(File file) {
-        File file01 = new File("Day14Task01.txt");
         try {
-            Scanner scanner = new Scanner(file01);
+            Scanner scanner = new Scanner(file);
             String line = scanner.nextLine();
             String[] numbersString = line.split(" ");
             if (numbersString.length != 10) {
                 throw new IOException();
             }
-            List<Integer> numbers = new ArrayList<>();
-            int sum = 0;
+            int[] numbersArrayStatic = new int[numbersString.length]; //Вариант с массивом
+            //List<Integer> numbersArrayDynamic = new ArrayList<>(); //Вариант с динамическим массивом
+            int counterStatic = 0;
+            int sumStatic = 0;
+            //int sumDynamic = 0;
             for (String number : numbersString) {
-                numbers.add(Integer.parseInt(number));
-                sum += Integer.parseInt(number);
+                numbersArrayStatic[counterStatic++] = Integer.parseInt(number);
+                //numbersArrayDynamic.add(Integer.parseInt(number));
+                sumStatic += Integer.parseInt(number);
+                //sumDynamic += Integer.parseInt(number);
             }
-            System.out.println(numbers);
-            System.out.println(sum);
+            System.out.println(Arrays.toString(numbersArrayStatic));
+            //System.out.println(numbersArrayDynamic);
+            System.out.println(sumStatic);
+            //System.out.println(sumDynamic);
             scanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("File is missing.");
--- src/Day14_Read_from_file_Exceptions/Task03/Test.java
+++
@@ -1,5 +0,0 @@
-package Day14_Read_from_file_Exceptions.Task03;
-
-public class Test {
-    private String name = "Pavel";
-}
