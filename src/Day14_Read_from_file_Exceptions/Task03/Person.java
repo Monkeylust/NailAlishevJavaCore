4941e3fd48ef8d4c82f56ede9043c646443682f5
Monkeylust <mo9keylust@mail.ru>
2024-11-15 21:57:32 +0300

    Перенёс в main декомпозировал

--- src/Day14_Read_from_file_Exceptions/Task03/Person.java
+++ src/Day14_Read_from_file_Exceptions/Task03/Person.java
@@ -1,11 +1,5 @@
 package Day14_Read_from_file_Exceptions.Task03;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Person {
     private String name;
     private int age;
@@ -26,29 +20,4 @@ public class Person {
 //    public String toString(){
 //        return "%s, %d" .formatted(name, age); //Помню, помню твои учения)
 //    }
-
-    public static List<Person> parseFileToObjList(){
-        File file = new File("Day14Task02People");
-        List<Person> listPerson = new ArrayList<>();
-        try(Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNextLine()) {
-                String linePerson = scanner.nextLine();
-                String[] arrayPerson = linePerson.split(" ");
-                validateAge(arrayPerson);
-                listPerson.add(new Person(arrayPerson[0], Integer.parseInt(arrayPerson[1])));
-            }
-            return listPerson;
-        } catch (FileNotFoundException e) {
-            System.out.println("File is missing.");
-        } catch (NegativeAgeException e) {
-            System.out.println(e.getMessage());
-        }
-        return new ArrayList<>();
-    }
-
-    public static void validateAge(String[] arrayPerson) throws NegativeAgeException {
-        if (Integer.parseInt(arrayPerson[1]) <= 0) {
-            throw new NegativeAgeException();
-        }
-    }
 }
