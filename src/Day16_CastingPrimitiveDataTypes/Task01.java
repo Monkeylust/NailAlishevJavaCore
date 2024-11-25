f9baca67ab41d372ed6890ddc73bb3593f225117
Monkeylust <mo9keylust@mail.ru>
2024-11-25 22:05:02 +0300

    Day15Task01

--- src/Day16_CastingPrimitiveDataTypes/Task01.java
+++ src/Day16_CastingPrimitiveDataTypes/Task01.java
@@ -15,17 +15,35 @@ public class Task01 {
             String[] numbers = scanner.nextLine().split(" ");
             double result = sum(numbers) / (double) numbers.length;
             System.out.println(Arrays.toString(numbers));
-            System.out.printf(result + " --> %.3f", result);
+            System.out.printf(result + " --> %.3f\n", result);
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("File is missing!");
         }
     }
 
     public static int sum(String[] numbers) {
         int sum = 0;
         for (String number : numbers) {
-            sum += Integer.parseInt(number);
+//            try {
+//                sum += Integer.parseInt(number);  //если без методов раскомментировать, доработка вне задания на развитие от Сереги
+//            } catch (NumberFormatException e) {
+//                System.out.println(number);
+//            }
+            if (isInteger(number)) {
+                sum += Integer.parseInt(number);
+            } else {
+                System.out.println("Исключение: " + number);
+            }
         }
         return sum;
     }
+
+    public static boolean isInteger(String str) { //метод делает проверку строки и игнорирует всё что не положительный int
+        return str != null && str.chars().allMatch(Character::isDigit);
+    }
+
+    public static boolean isNumber(String str) {
+        String regex = "^-?\\d+$";                  //че тут происходит я хз, но этот метод считает отрицательные числа
+        return str.matches(regex);                  //и не крашится если записать букву в файл или символ, плюс считает
+    }                                               //отрицательные числа
 }
