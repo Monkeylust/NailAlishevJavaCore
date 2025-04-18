6af95a99564a470e946c37431848bdd4391b3442
Monkeylust <mo9keylust@mail.ru>
2025-04-18 15:57:52 +0300

    Day16Task02 На ревью

--- src/Day16_CastingPrimitiveDataTypes/Task02.java
+++ src/Day16_CastingPrimitiveDataTypes/Task02.java
@@ -6,62 +6,90 @@ import java.io.PrintWriter;
 import java.util.*;
 
 public class Task02 {
+    static final String PATH_NAME_FILE_ONE = "file1";
+    static final String PATH_NAME_FILE_TWO = "file2";
+    static File fileTwo = new File(PATH_NAME_FILE_TWO);
+    static Random random = new Random();
+    static int counterStart = 0;
+    static int counterFinal = 15;
+
     public static void main(String[] args) {
-        try (PrintWriter pw1 = new PrintWriter("file1");
-             Scanner scanner1 = new Scanner(new File("file1"));
-             PrintWriter pw2 = new PrintWriter("file2")) {
-            StringBuilder sb1 = new StringBuilder();
-            StringBuilder sb2 = new StringBuilder();
-            createPw1(sb1, pw1);
-            String[] lineNumbersFile01 = scanner1.nextLine().split(" ");
-            createPw2(lineNumbersFile01, sb2, pw2);
-            printResult(new File("file2"));
+        try (PrintWriter pwOne = new PrintWriter("file1");
+             Scanner scannerOne = new Scanner(new File(PATH_NAME_FILE_ONE));
+             PrintWriter pwTwo = new PrintWriter(fileTwo)) {
+            createPwOne(pwOne);
+            if (scannerOne.hasNext()) {
+                String[] lineNumbersFileOne = scannerOne.nextLine().split(" ");
+                printPwTwo(lineNumbersFileOne, pwTwo);
+            } else {
+                throw new NoSuchElementException();
+            }
+            printResult(fileTwo);
         } catch (FileNotFoundException e) {
             System.out.println("File is missing!");
+        } catch (NoSuchElementException e) {
+            System.out.println("File is empty");
         }
     }
 
-    public static void createPw1(StringBuilder sb1, PrintWriter pw1) {
-        Random random = new Random();
-        for (int i = 0; i < 15; i++) {
-            sb1.append(random.nextInt(100)).append(" ");
+    public static void createPwOne(PrintWriter pwOne) {
+        StringBuilder sbOne = new StringBuilder();
+        for (int i = counterStart; i < counterFinal; i++) {
+            sbOne.append(random.nextInt(100)).append(" ");
         }
-        pw1.print(sb1);
-        pw1.flush();
+        pwOne.print(sbOne);
+        pwOne.flush();
     }
 
-    public static void createPw2(String[] lineNumbersFile01, StringBuilder sb2, PrintWriter pw2) {
+    public static void printPwTwo(String[] lineNumbersFileOne, PrintWriter pwTwo) {
+        StringBuilder sbTwo = new StringBuilder();
         int sum = 0;
         int counter = 0;
         StringBuilder sbTemp = new StringBuilder();
-        for (String string : lineNumbersFile01) {
+        for (String string : lineNumbersFileOne) {
+            if (!isNumber(string)) {
+                continue;
+            }
             sum += Integer.parseInt(string);
             counter++;
-            if (counter == 5) {
-                sb2.append(sum / 5.0).append(" ");
+            if (counter >= 5) {
+                sbTwo.append(sum / 5.0).append(" ");
                 sbTemp.append(sum).append(" ");
                 sum = 0;
                 counter = 0;
             }
         }
-        System.out.println(Arrays.toString(lineNumbersFile01));
+        System.out.println(Arrays.toString(lineNumbersFileOne));
         System.out.println(sbTemp);
-        System.out.println(sb2);
-        pw2.print(sb2);
-        pw2.flush();
+        System.out.println(sbTwo);
+        pwTwo.print(sbTwo);
+        pwTwo.flush();
     }
 
-    public static void printResult(File file02) {
-        try (Scanner scanner = new Scanner(file02)) {
-            String[] line = scanner.nextLine().split(" ");
-            double result = 0;
-            for (String string : line) {
-                result += Double.parseDouble(string);
+    public static void printResult(File fileTwo) {
+        try (Scanner scanner = new Scanner(fileTwo)) {
+            if (scanner.hasNext()) {
+                String[] line = scanner.nextLine().split(" ");
+                double result = 0;
+                for (String string : line) {
+                    if (!isNumber(string)) {
+                        continue;
+                    }
+                    result += Double.parseDouble(string);
+                }
+                System.out.println(result);
+                System.out.println((int) result);
+            } else {
+                throw new NoSuchElementException();
             }
-            System.out.println(result);
-            System.out.println((int) result);
         } catch (FileNotFoundException e) {
             System.out.println("File is missing!");
+        } catch (NoSuchElementException e) {
+            System.out.println("File is empty");
         }
     }
- }
+
+    public static boolean isNumber(String string) {
+        return string.matches("-?\\d+");
+    }
+}
