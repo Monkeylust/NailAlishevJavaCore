6915846c2b10bbd9ad5f720bddb0edc1a9e631d7
Monkeylust <mo9keylust@mail.ru>
2025-05-08 15:35:46 +0300

    Day18Task1Recursion На ревью final

--- src/Day18_Recursion/Task1.java
+++ src/Day18_Recursion/Task1.java
@@ -6,8 +6,8 @@ public class Task1 {
     static Random random = new Random();
 
     public static void main(String[] args) {
-        int[] randomArray = randomArray();
-        int sum = recursionSum(randomArray, 0);
+        int[] array = getRandomArray();
+        int sum = recursionSum(array, 0);
         System.out.printf("%s %d \n", "Сумма чисел массива =", sum);
     }
 
@@ -19,7 +19,7 @@ public class Task1 {
         return array[index] + recursionSum(array, index + 1);
     }
 
-    private static int[] randomArray() {
+    private static int[] getRandomArray() {
         int[] array = new int[3];
         for (int i = 0; i < array.length; i++) {
             array[i] += random.nextInt(100);
